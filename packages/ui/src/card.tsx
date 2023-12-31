5e1f825059c7e42d959c5b3ab13b44563b3af905
devsargam <sargampoudel100@gmail.com>
2024-01-01 02:37:44 +0530

    feat(ui): setup ui with `tailwind`

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,25 +1,23 @@
-export function Card({
-  className,
-  title,
-  children,
-  href,
-}: {
-  className?: string;
+import * as React from 'react';
+
+type CardProps = {
   title: string;
   children: React.ReactNode;
   href: string;
-}): JSX.Element {
+};
+
+export const Card: React.FC<CardProps> = ({ title, children, href }) => {
   return (
-    <a
-      className={className}
-      href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <h2>
-        {title} <span>-&gt;</span>
+    <>
+      <h2 className="ui-mb-3 ui-text-2xl ui-font-semibold">
+        {title}{' '}
+        <span className="ui-inline-block ui-transition-transform group-hover:ui-translate-x-1 motion-reduce:ui-transform-none">
+          -&gt;
+        </span>
       </h2>
-      <p>{children}</p>
-    </a>
+      <p className="ui-m-0 ui-max-w-[30ch] ui-text-sm ui-opacity-50">
+        {children}
+      </p>
+    </>
   );
-}
+};
