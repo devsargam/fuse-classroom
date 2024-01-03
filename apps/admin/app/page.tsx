500f7d1e6692a202d5e8572d1df9be2a2ea42d9d
devsargam <sargampoudel100@gmail.com>
2024-01-03 10:58:38 +0530

    feat(db): add database in `web` & `admin`

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,4 +1,11 @@
-export default function Page(): JSX.Element {
+import { PrismaClient } from '@repo/database';
+
+const client = new PrismaClient();
+
+export default async function Page(): Promise<JSX.Element> {
+  const results = await client.user.findMany();
+
+  console.log(results);
   return (
     <main>
       <h1 className="text-blue-500">This is admin page</h1>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,17 @@
 // import { Card } from '@repo/ui/card';
 import React from 'react';
+import { PrismaClient } from '@repo/database';
 
-export default function Page(): JSX.Element {
+export default async function Page(): Promise<JSX.Element> {
+  const client = new PrismaClient();
+  const res = await client.user.findMany();
+
   return (
     <main>
       {/* <Card href="https://test.com" title="something">
         Hello World
       </Card> */}
-      <h1 className="text-blue-700">hello world</h1>
+      <h1 className="text-blue-700">{JSON.stringify(res)}</h1>
     </main>
   );
 }
